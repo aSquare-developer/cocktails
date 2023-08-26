43047a12b5d3e2d8f48bbe7f2d19bc317e889260
Artur-MacBook-Air <artur@example.com>
2023-08-26 14:46:14 +0300

    Configuring Vue Router

---
+++ src/pages/CocktailRandom.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+Cocktail Random
+</template>
+
+<style lang="sass" scoped>
+@import "../assets/styles/main"
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+Home
+</template>
+
+<style lang="sass" scoped>
+@import "../assets/styles/main"
+
+</style>
